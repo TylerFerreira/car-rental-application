0484616aeec9ed7bd73e0308628d2786d80fa1a3
Tyler Ferreira <tyferreira4@hotmail.com>
2023-04-27 01:18:11 -0400

    Create components

---
+++ src/components/SideNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SideNav() {
+  return (
+    <div className="sidenav">
+      <a href="#">Home</a>
+      <a href="#">Cars</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </div>
+  );
+}
+
+export default SideNav;
